a0516d7c2dc6ee0a896f2b7265e1acc543e97521
aferaudo <aferaudo34@gmail.com>
2022-09-06 17:52:38 +0200

    updated wifi analysis script

--- wifi_crowding_bologna/outdoor_wifi_analyser.py
+++ wifi_crowding_bologna/outdoor_wifi_analyser.py
@@ -1,14 +1,42 @@
 from asyncio.base_subprocess import ReadSubprocessPipeProto
+from dataclasses import field
 import requests
 import argparse
 from datetime import datetime, timedelta
 import urllib.parse as urlParser
+import geopandas
+from shapely.geometry import Polygon
+import pandas
+
+gdf = geopandas.read_file("geometry.json") # change geometry.json to change polygon
+
+output_data = {
+    "nome_zona" : [],
+    "data" : [],
+    "ora" : [],
+    "affollamento_medio" : []
+}
+
+def elaborate_results(records):
+    for record in records:
+        values = record["record"]["fields"]
+        print("Analysing {}".format(values["nome_zona"]))
+        polygon_geom = Polygon(values["geo_shape"]["geometry"]["coordinates"][0])
+        gdf1 = geopandas.GeoDataFrame({'geometry':[polygon_geom]}).set_crs(4326)
+        # df1 = df1.to_crs()
+        # print(df["geometry"])
+        if gdf["geometry"].intersects(gdf1["geometry"])[0]:
+            output_data["nome_zona"].append(values["nome_zona"])
+            output_data["data"].append(values["data"])
+            output_data["ora"].append(values["ora"])
+            output_data["affollamento_medio"].append(values["affollamento_medio"])
+    
 
 
 
 def main(argv):
 
-    basic_url = "https://opendata.comune.bologna.it/api/v2/catalog/datasets/iperbole-wifi-affollamento/records?where="
+    basic_url = "https://opendata.comune.bologna.it/api/v2/catalog/datasets/iperbole-wifi-affollamento/records?select=geo_shape%2C%20nome_zona%2C%20data%2C%20ora%2C%20affollamento_medio&&timezone=Europe%2FBerlin&where="
     
     datetime_str = '2022-07-01 00:00:00'
     datetime_s_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
@@ -20,10 +48,37 @@ def main(argv):
         start_date = datetime_s_object.isoformat()
         datetime_s_object += timedelta(days=1)
         end_date = (datetime_s_object - timedelta(seconds=1)).isoformat()
-        print(end_date)
+        
+        # First query
         query = urlParser.quote("data>=date'" + start_date + "' and data<=date'" +end_date + "'")+ "&sort=data" # We need to specify other facet
         final_url = basic_url + query
-        print(final_url)
+        response = requests.get(final_url)
+
+        elaborate_results(response.json()["records"])
+        link_next_page = "init" # simulating do while
+        # Finding link for the next page (do it until the last page)
+        while link_next_page:
+            link_next_page = ""
+            links = response.json()["links"] # it is a list of element
+            for link in links:
+                # Each link is a dict with the following keys:
+                # rel: 'last', 'previous', 'next', 'self', 'first'
+                # href: link
+                if link['rel'] == 'next':
+                    link_next_page = link['href']
+                    break
+            if not link_next_page:
+                print("No next page")
+                break
+            response = requests.get(link_next_page)
+            elaborate_results(response.json()["records"])
+
+
+
+    df = pandas.DataFrame(data=output_data)
+
+    df.to_csv("data_iperbole_test.csv", sep=";")
+            
 
     
     
