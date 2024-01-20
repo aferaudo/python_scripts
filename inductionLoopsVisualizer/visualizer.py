ab51c9cbbf5c489256165efdf63a207fd82d9e1e
aferaudo <aferaudo34@gmail.com>
2024-01-20 17:25:49 +0100

    inductionLoopsVisualizer

---
+++ inductionLoopsVisualizer/visualizer.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+
+def main():
+    
+    df = pd.read_csv("config/dataset.csv", sep=";")
+    df = df.dropna()
+    px.set_mapbox_access_token(open("config/.mapbox_token").read())
+
+    df = df.groupby(["ID_univoco_stazione_spira", "geopoint"]).size().reset_index(name="count")
+    df[['lat', 'longi']] = df['geopoint'].str.split(', ', expand=True)
+    df['lat'] = pd.to_numeric(df['lat'], errors='coerce')
+    df['longi'] = pd.to_numeric(df['longi'], errors='coerce')
+    print(df.dtypes)
+    print(df.iloc[0])    
+    print(df.head())
+    # df1 = px.data.gapminder().query("year == 2007")
+    # print(df1.head())
+    fig = px.scatter_mapbox(df,
+                    lat=df.lat,
+                    lon=df.longi,
+                    hover_name="ID_univoco_stazione_spira")
+    fig.show()
+
+if __name__ == '__main__':
+    main()
